0108a16b8a5e8e166f4ce386d32889cc5a611cb8
ellie <elliehuhta@gmail.com>
2022-06-30 23:47:39 -0400

    added function for random generators

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,41 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumeric() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+function getRandomSpecial() {
+  const symbols = '~!@#$%^&*()=<>?/.,":[]{}';
+  return symbols[0];
+}
 
+const randomFunc = {
+  lower: getRandomLower
+  upper: getRandomUpper
+  numeric: getRandomNumeric
+  special: getRandomSpecial
+  }
+function generatePassword() {
+  //Password Requirments?
+
+    //prompt to ask about length of password, between 8 and 128
+
+    //prompt to ask: include numeric?
+    //prompt to ask: include special characters?
+    //prompt to ask: include lowercase letters?
+    //prompt to ask: include uppercase letters?
+
+  //validate the input
+
+  //Generate password based on criteria
+
+  //Display password on page
 return "Generated password will be here";
 }
 
